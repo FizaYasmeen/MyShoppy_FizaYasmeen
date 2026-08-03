ab9950da994d06582b1202599cb4e385bc0028e1
Fiza Yasmeen <152010285+FizaYasmeen@users.noreply.github.com>
2026-08-03 05:35:15 +0530

    Updated Cart.tsx

--- src/navigation/Cart.tsx
+++ src/navigation/Cart.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
 import CartScreen from "../screens/CartScreen";
 import CustomerDetailsScreen from "../screens/CustomerDetailsScreen";
 import ConfirmationScreen from "../screens/ConfirmationScreen";
